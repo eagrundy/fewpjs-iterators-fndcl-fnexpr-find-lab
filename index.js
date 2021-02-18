b9b22a4b4edf2989c65453565f0ff5f7f4e87d7d
eagrundy <ericamalakian@gmail.com>
2021-02-18 11:56:21 -0800

    commit tree 7367f71668b45648eae324c9d2bbbeb4794c4bae

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let find = array.find(function(object) {return object.result === "W"})
+
+  if (find) {
+    return find.year
+  }
+  else {
+    undefined
+  }
+}
+
